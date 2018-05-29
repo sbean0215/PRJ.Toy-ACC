3ba88893adca5fa0ae28b7d6cf84190900fa0285
bean <sbean0215@gmail.com>
2018-05-29 14:49:02 +0900

    initial commit

---
+++ app/src/main/java/com/example/bean/studyforaac/model/NewsPieceItem.java
@@ -0,0 +1,8 @@
+package com.example.bean.studyforaac.model;
+
+public interface NewsPieceItem {
+    String getTitle();
+    String getContents();
+    String getTime();
+    String getTitleImgeUrl();
+}
